55e598d57ec06b6d617b37efc422c95cad625f68
Michel Fedde <Fedde-M@schueler.ers-hm.de>
2020-07-31 14:10:07 +0200

    Added SMRenderer & OpenTK & Created window

---
+++ Gamejam 2020/Gamejam 2020/GameWindow.cs
@@ -0,0 +1,10 @@
+﻿using SM.Core.Enums;
+using SM.Core.Plugin;
+
+namespace Gamejam_2020
+{
+    public class GameWindow : WindowPlugin
+    {
+        public override WindowUsage NeededUsage { get; } = WindowUsage.All;
+    }
+}
--- Gamejam 2020/Gamejam 2020/Program.cs
+++ Gamejam 2020/Gamejam 2020/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SM;
+using SM.Core.Enums;
+using SM.Core.Window;
 
 namespace Gamejam_2020
 {
@@ -10,6 +13,9 @@ namespace Gamejam_2020
     {
         static void Main(string[] args)
         {
+            GLWindow window = new GLWindow(new WindowSettings(.9f, "Gamejam 2020 - Testing Build"), new GLInformation());
+            window.Use(WindowUsage.All, new DefaultWindow(), new GameWindow());
+            window.Run();
         }
     }
 }
