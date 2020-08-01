b603be4f9fba9dae329e6963bdad0cb6f90793a9
Michel Fedde <Fedde-M@schueler.ers-hm.de>
2020-08-01 12:23:10 +0200

    Backgrounds Again

---
+++ Gamejam 2020/Gamejam 2020/Background.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL4;
+using SM;
+using SM.Animations;
+using SM.Core;
+using SM.Core.Window;
+using SM.Data.Models;
+using SM.Data.Types.VectorTypes;
+using SM.Scene.Cameras;
+using SM.Scene.Draw;
+
+namespace Gamejam_2020
+{
+    public class Background : SMItemCollection
+    {
+        private const float Width = 5f;
+        private const float Height = 5f;
+
+        private DrawCall call;
+
+        public Background()
+        {
+            Camera = new PerspectiveCamera();
+            DepthFunc = DepthFunction.Less;
+
+            call = new DrawCall
+            {
+                Material = {DiffuseColor = new Color(1,1,1)},
+                Mesh = Models.Cube
+            };
+            Add(call);
+
+            Timer timer = new Timer(1/5f, true)
+            {
+                Repeat = true
+            };
+            timer.End += TimerSys;
+            timer.Start();
+        }
+
+        private void TimerSys(Timer timer)
+        {
+            Position pos = new Position
+            {
+                X = (float) (SMGlobals.Randomizer.NextDouble() * (Width * 2) - Width),
+                Y = (float) (SMGlobals.Randomizer.NextDouble() * (Height * 2) - Height),
+                Z = 5f
+            };
+
+            CallParameter parameter = new CallParameter()
+            {
+                Position = pos,
+                Size = new Size((float)SMGlobals.Randomizer.NextDouble() * .1f)
+            };
+            call.DrawCallParameters.Add(parameter);
+            Animation animation = new Animation(pos, new AnimationStruct(TimeSpan.FromSeconds(5), false,  pos, new AnimationVector(pos.X, pos.Y, -1)), false);
+            animation.End += a => call.DrawCallParameters.Remove(parameter);
+            animation.Start();
+        }
+    }
+}
---
+++ Gamejam 2020/Gamejam 2020/GameScene.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK;
+using SM.Data.Models;
+using SM.Data.Types.VectorTypes;
+using SM.Scene;
+using SM.Scene.Cameras;
+using SM.Scene.Draw;
+using SM.Scene.Lights;
+
+namespace Gamejam_2020
+{
+    public class GameScene : Scene
+    {
+        DrawObject ship = new DrawObject()
+        {
+            Mesh = Models.Spaceship,
+            Material = new Material(){DiffuseColor = new Color(1,0,0)}
+        };
+        DrawObject pyramid = new DrawObject()
+        {
+            Mesh = Models.Pyramid,
+            Position = new Position(0, z:5)
+        };
+        DrawObject cube = new DrawObject()
+        {
+            Mesh = Models.Cube,
+            Position = new Position(0, z: 10)
+        };
+        DrawObject tri = new DrawObject()
+        {
+            Mesh = Models.Triangle,
+            Position = new Position(0, z: 15)
+        };
+
+        public GameScene()
+        {
+            Lights.Ambient = new Color(.1f,.1f,.1f);
+            Lights.Add(new Sun(new Vector3(0,-1,-5)));
+
+            Background = new Background();
+        }
+    }
+}
